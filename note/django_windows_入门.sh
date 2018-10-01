609b5a3fe66417fb7147a7661c2eef1b716a1d26
CDONG <chuanpengdong@gmail.com>
2018-10-01 16:19:46 -0400

    Update django_windows_入门.sh

--- note/django_windows_入门.sh
+++ note/django_windows_入门.sh
@@ -14,7 +14,13 @@ pip install django
 
 deactivate
 
+# https://borisliu.gitbooks.io/from-python-to-django/content/django-step-by-step/chapter01.html
+# 第一讲.从简单到复杂.从Python到Django入门教程
+
 # https://mp.weixin.qq.com/s?src=11&timestamp=1538424867&ver=1156&signature=xvIghhCRM0AtDPd-AKlYUZLK8fiBrrgINxf33SITLQbSFQfFD3evOmz66JEcg8qvPV0z3v2PEHQpmct4ynLp6CzdItMMMVlRYp9ln8ikFjxwcLGh9tpOqMNndMGaN76H&new=1
 # 一个完整的初学者指南Django - 第1部分
 # https://mp.weixin.qq.com/s?__biz=MjM5MzgyODQxMQ==&mid=2650368035&idx=1&sn=c5573cd65c918e3f963949ae3e6dbe93&chksm=be9cd37789eb5a61a0f02ebee52fee9e3d4ffedf9640588cb665c5e8ad2aeddf035dbc160bf1&scene=21#wechat_redirect
 # Django入门指南-第2部分（系统设计)
+
+# https://mp.weixin.qq.com/s?src=11&timestamp=1538413014&ver=1156&signature=hLdwb-E6cfU6UpoXxiQmjXXgUABnfvQGnJ-Aw3TwMmHa5jZtIGwCvd3mFHWahTlwQQGepyb3XAQn1JXGpRPHpKKVFr*avUAHjRfZZzRXQ1GKebB8g14n-ke8*WgIWJ7W&new=1
+# 小白学Python，最简单的Django 教程 
