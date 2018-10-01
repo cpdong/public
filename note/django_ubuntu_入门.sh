954fe323005b3463a02a25fd00d502ba8dc995c3
CDONG <chuanpengdong@gmail.com>
2018-10-01 13:31:54 -0400

    Update django_ubuntu_入门.sh

--- note/django_ubuntu_入门.sh
+++ note/django_ubuntu_入门.sh
@@ -1,2 +1,10 @@
 # 
+sudo pip install virtualenv virtualenvwrapper
+
+# how to use virtualenv
+virtualenv --no-site-package myenv
+python -m virtualenv myvenv
+source myvenv/bin/activate
+
+
 django-admin startproject mysite
