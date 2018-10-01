f3e7d1105420a6dfc468d41a3798a32fc003b9fd
CDONG <chuanpengdong@gmail.com>
2018-10-01 16:16:39 -0400

    Update django_windows_入门.sh

--- note/django_windows_入门.sh
+++ note/django_windows_入门.sh
@@ -5,9 +5,16 @@
 pip install virtualenvwrapper-win
 
 # https://docs.djangoproject.com/en/2.1/howto/windows/
+# make a new prject
 mkvirtualenv myproject
+
 workon myproject
 
 pip install django
 
 deactivate
+
+# https://mp.weixin.qq.com/s?src=11&timestamp=1538424867&ver=1156&signature=xvIghhCRM0AtDPd-AKlYUZLK8fiBrrgINxf33SITLQbSFQfFD3evOmz66JEcg8qvPV0z3v2PEHQpmct4ynLp6CzdItMMMVlRYp9ln8ikFjxwcLGh9tpOqMNndMGaN76H&new=1
+# 一个完整的初学者指南Django - 第1部分
+# https://mp.weixin.qq.com/s?__biz=MjM5MzgyODQxMQ==&mid=2650368035&idx=1&sn=c5573cd65c918e3f963949ae3e6dbe93&chksm=be9cd37789eb5a61a0f02ebee52fee9e3d4ffedf9640588cb665c5e8ad2aeddf035dbc160bf1&scene=21#wechat_redirect
+# Django入门指南-第2部分（系统设计)
