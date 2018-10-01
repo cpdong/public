9fbaa525b4c47691c1553e043a4966c14aa593ff
CDONG <chuanpengdong@gmail.com>
2018-10-01 15:24:39 -0400

    Create django_windows_入门.sh

---
+++ note/django_windows_入门.sh
@@ -0,0 +1,13 @@
+# 安装python并改变环境变量path. python与子目录scripts。
+
+# 安装pip gz包解压 https://www.bbsmax.com/A/1O5EeBmr57/。
+
+pip install virtualenvwrapper-win
+
+# https://docs.djangoproject.com/en/2.1/howto/windows/
+mkvirtualenv myproject
+workon myproject
+
+pip install django
+
+deactivate
