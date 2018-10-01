c25f0deb9405f4832a4759652d031fa41305bca1
CDONG <chuanpengdong@gmail.com>
2018-10-01 15:24:53 -0400

    Update django_windows_入门.sh

--- note/django_windows_入门.sh
+++ note/django_windows_入门.sh
@@ -1,6 +1,6 @@
 # 安装python并改变环境变量path. python与子目录scripts。
 
-# 安装pip gz包解压 https://www.bbsmax.com/A/1O5EeBmr57/。
+# 安装pip gz包解压 https://www.bbsmax.com/A/1O5EeBmr57/
 
 pip install virtualenvwrapper-win
 
