56b045096981619e5280a4031efbfa0f372aba47
CDONG <chuanpengdong@gmail.com>
2018-10-15 15:15:16 -0400

    Update Regulons_activity_all_in_ONE.R

--- code/projects/CAN-EXP/Regulons_activity_all_in_ONE.R
+++ code/projects/CAN-EXP/Regulons_activity_all_in_ONE.R
@@ -2,7 +2,7 @@
 #############################################################
 # GSE_ID= "GSE17907"
 # platform= "GPL570"
-# acu_percentage=0.2
+# acu_percentage=0.15
 # dir="/home/cpdong/Desktop/demo/"
 #############################################################
 # source("https://raw.githubusercontent.com/cpdong/public/master/code/projects/CAN-EXP/Regulons_activity_all_in_ONE.R")
