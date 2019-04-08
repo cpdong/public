7bd9583202acf8243726ba072e891846c16a51c9
CDONG <chuanpengdong@gmail.com>
2019-04-08 14:58:48 -0400

    Create ENCODE_bed_select.R

---
+++ code/ENCODE_bed_select.R
@@ -0,0 +1,28 @@
+########################################################
+# R function
+# Donwload online data that hidden in the html source
+
+audit_status<- function(fileURL){
+
+    # Get download link from http links
+    ####################################################
+    library(stringr)
+    library(rvest)
+    library(XML)
+    library(RCurl)
+
+    link <- getURL(fileURL,.encoding='utf-8')
+    doc <-htmlParse(link)
+    nodess<- getNodeSet(doc, "//script")
+
+    # Get NODE content with XML
+    node_content<- xpathSApply(context,"//script",xmlValue)[3]
+
+    # Extract the substrings: https://stackoverflow.com/questions/39086400/extracting-a-string-between-other-two-strings-in-r
+    stingss<- str_match(as.character(node_content), "biological_replicates(.*?)assembly")[,2]
+    bio_rep<- str_extract_all(strings,"\\(?[0-9,.]+\\)?")[[1]][1]
+
+    return(bio_rep)
+}
+########################################################
+# Function END
