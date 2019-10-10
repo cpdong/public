7724bb5ada39dbff4abb830f7a0c552df753227c
CDONG <chuanpengdong@gmail.com>
2019-10-10 15:44:55 -0400

    Update limma_DE_two_condition_comparison.R

--- code/limma_DE_two_condition_comparison.R
+++ code/limma_DE_two_condition_comparison.R
@@ -1,18 +1,34 @@
+setwd("C:/Users/Chuanpeng/Desktop/Andy")
+
+###################### process the data into shape with Norm(# N)--AD(# N)
+df1<- read.csv("merge_data_MAYO_TCX_TCX.csv", header=F, row.names=1, stringsAsFactors=F)
+df2<- df1[-1,]
+# find the normal sample
+df_normal<- df2[, which(df2[7,] =="TCX.CONTROL")]
+normN<- dim(df_normal)[2]
+# find the disease sample
+df_disease<- df2[, which(df2[7,] =="TCX.AD")]
+diseaseN<- dim(df_disease)[2]
+
+dataProcessed=cbind(rownames(df2), df_normal,df_disease)
+
+write.table(dataProcessed, paste("data_MAYO_TCX_limma_feed_Norm", normN, "_disease",diseaseN,".csv", sep=''), row.names=F, sep=',', col.names=F)
+#
+
+####################### start run limma different expression analysis
 library(limma)
-library(edgeR)
+exprs_Data<-data.frame(dataProcessed, row.names=1)
+exprs_Data=expression_Data[-c(1:18), ] # remove all the 18 clinical data rows 
+exprs_Data<- data.matrix(exprs_Data)
 
-exprs_data<- read.table(file = "logCPM_normalized_expression_matrix.txt", sep = "\t", header = TRUE, row.names = 1, stringsAsFactors = FALSE)
-# set labels that corresponse to conditions
-
-phenotype_row<- apply(phenotype_row, 2, function(x) ifelse(x > AD_score_cutoff, "AD", "Ctrl"))
-condition = factor(phenotype_row, levels=c("AD","Ctrl"), labels=c("AD", "Ctrl"))
-
+condition=factor(c(rep("N",71),rep("D",80)))
 # build a contrast matrix
 design<-model.matrix(~-1+condition)
 colnames(design)<-c("Control","Disease")
 contranst.matrix<-makeContrasts(contrasts="Disease-Control",levels=design)
+
 # linear fit
-fit<-lmFit(eset,design)
+fit<-lmFit(exprs_Data,design)
 # Compute Contrasts from Linear Model Fit
 fit1<-contrasts.fit(fit,contranst.matrix)
 # conduct bayes test
@@ -22,7 +38,7 @@ fit2<-eBayes(fit1)
 dif<-topTable(fit2,coef="Disease-Control",n=nrow(fit2),adjust="BH")
 genesymbol<-rownames(dif)
 dif<-cbind(genesymbol,dif)
-# write.table(dif,file="All_gene_DE_result.txt",sep='\t',quote=F,row.names=F)
+write.table(dif,file="All_gene_DE_result.txt",sep='\t',quote=F,row.names=F)
 
 # Output DE gene that satisfy lfc=log2(#) large than cutoff and FDR large than cutoff 
 # log2(#) # replace to the fold change you want set
