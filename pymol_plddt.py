00b4cf796b83011ee3257ca3e9da312d083a1541
Chuanpeng <chuanpengdong@gmail.com>
2022-12-31 10:39:15 -0500

    Update pymol_plddt.py

--- pymol_plddt.py
+++ pymol_plddt.py
@@ -7,11 +7,17 @@ Created on Sun Jan 01 2023
 
 from pymol import cmd
 
+set_color conf1, [0, 83, 214] #50
+set_color conf2, [101, 203, 243] #50-70
+set_color conf3, [255, 219, 19] #70-90
+set_color conf4, [255, 125, 69] #90
+
 def plddt(selection="all"):
-    cmd.color("#0053D6", f"({selection}) and b > 90")
-    cmd.color("#65CBF3", f"({selection}) and b < 90 and b > 70")
-    cmd.color("#FFDB13", f"({selection}) and b < 70 and b > 50")
-    cmd.color("#FF7D45", f"({selection}) and b < 50")
+    cmd.color("conf4", f"({selection}) and b > 90")
+    cmd.color("conf3", f"({selection}) and b < 90 and b > 70")
+    cmd.color("conf2", f"({selection}) and b < 70 and b > 50")
+    cmd.color("conf1", f"({selection}) and b < 50")
     
 cmd.extend("plddt", plddt)
 cmd.auto_arg[0]["plddt"] = [cmd.object_sc, "object", ""]
+#
